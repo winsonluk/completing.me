68253fdf81829d3ed00b5eff4289148e92d86d3b
Winson <winson.luk@gmail.com>
2018-01-22 03:32:42 -0800

    Replace default header with title bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
 import {List, ListItem} from 'material-ui/List';
-import logo from './logo.svg';
 import './App.css';
 
 const cities = [
@@ -59,13 +59,10 @@ class App extends Component {
     return (
       <MuiThemeProvider>
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AppBar
+          title="React Autocomplete"
+          showMenuIconButton={false}
+        />
         <TextField
           ref={focusInput}
           hintText="hint text"
