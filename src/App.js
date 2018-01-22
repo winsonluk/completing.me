7d43f7981e9ff85c26b5e41573d2da7010d9d11c
Winson <winson.luk@gmail.com>
2018-01-22 01:08:32 -0800

    Installed Material UI styling to node_modules and added a placeholder Material UI text field to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>Welcome to React</h2>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <TextField
+          hintText="hint text"
+          floatingLabelText="floating label text"
+        />
       </div>
+      </MuiThemeProvider>
     );
   }
 }
