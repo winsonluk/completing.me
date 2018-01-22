099759d6a334907d530368410e398030dcbb47b5
Winson <winson.luk@gmail.com>
2018-01-22 06:30:17 -0800

    Fix secondaryTextLines bug

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
           id="matchError"
           style={{display: 'none'}}
           secondaryText="Sorry, I couldn't find any matches for you. Perhaps try a different city?"
-          secondaryTextLines="2"
+          secondaryTextLines={2}
           disabled={true}
         />
       </List>
@@ -100,7 +100,7 @@ class App extends Component {
         <ListItem
           id="minError"
           secondaryText="Please type at least 3 characters so that I can find your city."
-          secondaryTextLines="2"
+          secondaryTextLines={2}
           disabled={true}
         />
       </List>
