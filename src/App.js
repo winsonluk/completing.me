41dab61a4c15a1d41a759c3af7e67edd62dbc748
Winson <winson.luk@gmail.com>
2018-01-22 05:25:44 -0800

    Add README and update domain name

--- src/App.js
+++ src/App.js
@@ -31,8 +31,15 @@ const cities = [
 ]
 
 class App extends Component {
+
+  // This function is called whenever the text input is changed.
   handleChange(e, match) {
+
+    // numCities begins at cities.length and decrements for every city that doesn't match.
+    // If numCities is 0, that means none of the cities match.
     var numCities = cities.length;
+
+    // Show/hide cities and errors based on matches.
     cities.forEach((city) => {
       if (match.length >= 3 && city.startsWith(match.toLowerCase())) {
         document.getElementById(city).style.display="block";
@@ -46,25 +53,37 @@ class App extends Component {
         document.getElementById("minError").style.display="block";
       }
     });
+
+    // If no cities match (numCities === 0), show a error message.
     if (match.length >= 3 && numCities === 0) {
         document.getElementById("matchError").style.display="block";
     } else {
         document.getElementById("matchError").style.display="none";
     }
   }
+
   render() {
+
+    // items[] contains the cities formatted as Material UI ListItems.
     let items = [];
     cities.forEach((city) => {
       items.push (
         <List style={{padding: '0px'}}>
           <ListItem
             id={city}
-            style={{display: 'none'}}
-            primaryText={city.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();})}
+            style={{display: 'none'}} // We want to hide the cities until they match the input.
+            // To preserve the format of cities[] while displaying properly capitalized results,
+            //  we do some simple string manipulation to the cities.
+            primaryText={city.replace(/\w\S*/g,
+                                      function(txt){return txt.charAt(0).toUpperCase()
+                                                    + txt.substr(1).toLowerCase();})}
           />
         </List>
       );
     });
+
+    // Add the error messages as ListItems, but format them differently than cities for ease of
+    // differentiation between cities and error messages.
     items.push (
       <List style={{padding: '0px'}}>
         <ListItem
@@ -84,14 +103,18 @@ class App extends Component {
         />
       </List>
     );
+
+    // Select the search field automatically when the page loads.
     const focusInput = input => {
       input.focus();
     };
+
+    // The page is formatted in the following order: App Bar, Text Field, ListItems.
     return (
       <MuiThemeProvider>
       <div className="App">
         <AppBar
-          title="React Autocomplete"
+          title="completing.me"
           showMenuIconButton={false}
         />
         <TextField
