219378d5ed32c44131df954fcacdbaac38016c81
Winson <winson.luk@gmail.com>
2018-01-22 04:11:23 -0800

    Implement error messages, replace placeholders

--- src/App.js
+++ src/App.js
@@ -32,13 +32,25 @@ const cities = [
 
 class App extends Component {
   handleChange(e, match) {
+    var numCities = cities.length;
     cities.forEach((city) => {
       if (match.length >= 3 && city.startsWith(match.toLowerCase())) {
         document.getElementById(city).style.display="block";
+        document.getElementById("minError").style.display="none";
+      } else if (match.length >= 3) {
+        numCities -= 1;
+        document.getElementById(city).style.display="none";
+        document.getElementById("minError").style.display="none";
       } else {
         document.getElementById(city).style.display="none";
+        document.getElementById("minError").style.display="block";
       }
     });
+    if (match.length >= 3 && numCities === 0) {
+        document.getElementById("matchError").style.display="block";
+    } else {
+        document.getElementById("matchError").style.display="none";
+    }
   }
   render() {
     let items = [];
@@ -53,6 +65,25 @@ class App extends Component {
         </List>
       );
     });
+    items.push (
+      <List style={{padding: '0px'}}>
+        <ListItem
+          id="matchError"
+          style={{display: 'none'}}
+          secondaryText="Sorry, I couldn't find any matches for you. Perhaps try a different city?"
+          disabled={true}
+        />
+      </List>
+    );
+    items.push (
+      <List style={{padding: '0px'}}>
+        <ListItem
+          id="minError"
+          secondaryText="Please type at least 3 characters so that I can find your city."
+          disabled={true}
+        />
+      </List>
+    );
     const focusInput = input => {
       input.focus();
     };
@@ -65,9 +96,9 @@ class App extends Component {
         />
         <TextField
           ref={focusInput}
-          hintText="hint text"
-          floatingLabelText="floating label text"
-          onChange={this.handleChange}
+          floatingLabelText="Search for a city with me!"
+          hintText="City"
+          onChange={this.handleChange.bind(this)}
         />
         {items}
       </div>
